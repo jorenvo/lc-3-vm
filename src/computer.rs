8302027665a6c80fd7af31560ef01f95d3754b96
Joren Van Onder <joren@jvo.sh>
2020-06-14 10:27:37 -0700

    Handle negative numbers

--- src/computer.rs
+++ src/computer.rs
@@ -21,13 +21,37 @@ impl Computer {
         }
     }
 
-    fn update_flags(&mut self, result: i16) {
-        self.registers[constants::RCOND] = match result {
-            result if result < 0 => constants::CONDNEGATIVE,
-            result if result == 0 => constants::CONDZERO,
-            result if result > 0 => constants::CONDPOSITIVE,
-            _ => panic!("What?"),
+    /// Sign extends an `n_bits`-bits number to 16 bits
+    ///
+    /// * `to_extend`: number to sign extend
+    /// * `n_bits`: `to_extend` is an `n_bits` bits number
+    fn sign_extend_to_16_bits(&self, to_extend: u16, n_bits: u16) -> u16 {
+        let sign_bit = to_extend >> (n_bits - 1);
+        let is_negative = sign_bit > 0;
+
+        // Sign extend 5 bit negative number to 8 bit example:
+        //    1 0000
+        // 1111 1111
+        if is_negative {
+            let mask: u16 = u16::MAX << (n_bits - 1);
+            to_extend | mask
+        } else {
+            to_extend
         }
+    }
+
+    fn is_negative(&self, a: u16) -> bool {
+        (a >> 15) > 0
+    }
+
+    fn update_flags(&mut self, result: u16) {
+        self.registers[constants::RCOND] = if self.is_negative(result) {
+            constants::CONDNEGATIVE
+        } else if result == 0 {
+            constants::CONDZERO
+        } else {
+            constants::CONDPOSITIVE
+        };
     }
 
     pub fn init_memory(&mut self, words: Vec<u16>) {
@@ -53,14 +77,15 @@ impl Computer {
                     let is_immediate_add = (instruction & 0b10_0000) > 0;
 
                     if is_immediate_add {
-                        let immediate_value = instruction & 0b1_1111; // TODO sign extend, negative numbers
+                        let immediate_value =
+                            self.sign_extend_to_16_bits(instruction & 0b1_1111, 5);
                         self.registers[dest_register] =
                             self.registers[src_register] + immediate_value;
                     } else {
                         panic!("Register add, not supported yet");
                     };
 
-                    self.update_flags(self.registers[dest_register] as i16);
+                    self.update_flags(self.registers[dest_register]);
                     println!("got add");
                 }
                 0b0101 => println!("got and"),
@@ -79,5 +104,28 @@ impl Computer {
             // go to next instruction
             self.registers[constants::RPC] += 1;
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::computer::Computer;
+
+    #[test]
+    fn test_sign_extend_positive() {
+        let c = Computer::new();
+        assert_eq!(
+            c.sign_extend_to_16_bits(0b0000_0000_0000_0100, 5),
+            0b0000_0000_0000_0100
+        );
+    }
+
+    #[test]
+    fn test_sign_extend_negative() {
+        let c = Computer::new();
+        assert_eq!(
+            c.sign_extend_to_16_bits(0b0000_0000_0001_0100, 5),
+            0b1111_1111_1111_0100
+        );
     }
 }
